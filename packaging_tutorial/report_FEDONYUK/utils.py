7ba77e02fe4a3838826fd41614dcb16877abaadf
Anatoliy 1974 <126827447+Anatoliy-Fedonyuk@users.noreply.github.com>
2023-07-18 15:47:23 +0200

    task 9 ver. 0.8

--- packaging_tutorial/report_FEDONYUK/utils.py
+++ packaging_tutorial/report_FEDONYUK/utils.py
@@ -1,20 +1,39 @@
-from packaging_tutorial.report_FEDONYUK.sqlite_creation import DriverModel
+from flask import current_app
+from sqlalchemy.orm import sessionmaker
+
+from packaging_tutorial.report_FEDONYUK.sqlite_creation import db, DriverModel
+
+Session = sessionmaker()
 
 
 def get_report(asc: bool = True, driver: str = None) -> list[list]:
     """Building an overall or separate report on the Monaco race F1 2018 from monaco.db"""
-    report = [driver.to_list() for driver in DriverModel.query.all()]
-    if driver:
-        report = [(DriverModel.query.filter_by(driver_id=driver).first()).to_list()]
-    if not asc:
-        report.reverse()
+    with current_app.app_context():
+        Session.configure(bind=db.engine)
+        session = Session()
+        query = session.query(DriverModel)
+
+        if driver:
+            query = query.filter_by(driver_id=driver)
+        if not asc:
+            query = query.order_by(DriverModel.id.desc())
+        report = [driver.to_list() for driver in query.all()]
+
+        session.close()
     return report
 
 
 def get_drivers(asc: bool = True) -> list[list]:
     """Building a list of drivers on the Monaco race F1 2018 from monaco.db"""
-    sort_model = sorted(DriverModel.query.all(), key=lambda x: x.name)
-    drivers = [[dr.name, dr.driver_id] for dr in sort_model]
-    if not asc:
-        drivers.reverse()
+    with current_app.app_context():
+        Session.configure(bind=db.engine)
+        session = Session()
+        query = session.query(DriverModel).order_by(DriverModel.name)
+
+        drivers = [[driver.name, driver.driver_id] for driver in query.all()]
+        if not asc:
+            drivers.reverse()
+
+        session.close()
     return drivers
+
