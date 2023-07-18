2ad077773ef466c554f5bccb340c7473dbd3d81a
Anatoliy 1974 <126827447+Anatoliy-Fedonyuk@users.noreply.github.com>
2023-07-18 10:25:29 +0200

    task 9 ver. 0.5

--- packaging_tutorial/report_FEDONYUK/report_web.py
+++ packaging_tutorial/report_FEDONYUK/report_web.py
@@ -55,22 +55,5 @@ def show_drivers():
     return render_template('drivers.html', drivers=get_drivers(asc))
 
 
-# def get_report(asc: bool = True, driver: str = None) -> list[list]:
-#     """Building an overall or separate report on the Monaco race F1 2018 from monaco.db"""
-#     report = [driver.to_list() for driver in DriverModel.query.all()]
-#     if driver:
-#         report = [(DriverModel.query.filter_by(driver_id=driver).first()).to_list()]
-#     if not asc: report.reverse()
-#     return report
-#
-#
-# def get_drivers(asc: bool = True) -> list[list]:
-#     """Building a list of drivers on the Monaco race F1 2018 from monaco.db"""
-#     sort_model = sorted(DriverModel.query.all(), key=lambda x: x.name)
-#     drivers = [[dr.name, dr.driver_id] for dr in sort_model]
-#     if not asc: drivers.reverse()
-#     return drivers
-
-
 if __name__ == '__main__':
     app.run(debug=True)
--- packaging_tutorial/report_FEDONYUK/utils.py
+++ packaging_tutorial/report_FEDONYUK/utils.py
@@ -6,7 +6,8 @@ def get_report(asc: bool = True, driver: str = None) -> list[list]:
     report = [driver.to_list() for driver in DriverModel.query.all()]
     if driver:
         report = [(DriverModel.query.filter_by(driver_id=driver).first()).to_list()]
-    if not asc: report.reverse()
+    if not asc:
+        report.reverse()
     return report
 
 
@@ -14,5 +15,6 @@ def get_drivers(asc: bool = True) -> list[list]:
     """Building a list of drivers on the Monaco race F1 2018 from monaco.db"""
     sort_model = sorted(DriverModel.query.all(), key=lambda x: x.name)
     drivers = [[dr.name, dr.driver_id] for dr in sort_model]
-    if not asc: drivers.reverse()
+    if not asc:
+        drivers.reverse()
     return drivers
