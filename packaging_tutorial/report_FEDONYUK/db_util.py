746b4eb1ea0373c57dd8ce2faccdc41f829ac4cc
Anatoliy 1974 <126827447+Anatoliy-Fedonyuk@users.noreply.github.com>
2023-07-19 20:16:12 +0200

    task 9 ver. 0.8

--- packaging_tutorial/report_FEDONYUK/db_util.py
+++ packaging_tutorial/report_FEDONYUK/db_util.py
@@ -4,12 +4,11 @@ from packaging_tutorial.report_FEDONYUK.models import db, DriverModel
 def get_report(asc: bool = True, driver: str = None) -> list[list]:
     """Building an overall or separate report on the Monaco race F1 2018 from monaco.db"""
     query = db.session.query(DriverModel).order_by(DriverModel.id)
-
     if driver:
         query = query.filter_by(driver_id=driver)
-    if not asc:
-        query = query.order_by(DriverModel.id.desc())
     report = [[dr.id, dr.driver_id, dr.name, dr.team, dr.best_lap] for dr in query.all()]
+    if not asc:
+        report.reverse()
 
     return report
 
@@ -17,7 +16,6 @@ def get_report(asc: bool = True, driver: str = None) -> list[list]:
 def get_drivers(asc: bool = True) -> list[list]:
     """Building a list of drivers on the Monaco race F1 2018 from monaco.db"""
     query = db.session.query(DriverModel).order_by(DriverModel.name)
-
     drivers = [[driver.name, driver.driver_id] for driver in query.all()]
     if not asc:
         drivers.reverse()
