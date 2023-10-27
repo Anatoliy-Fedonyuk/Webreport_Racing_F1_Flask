c9d4fed2f29981c5430be5f0ca8fcb0cb3248799
Anatoliy 1974 <126827447+Anatoliy-Fedonyuk@users.noreply.github.com>
2023-10-27 10:25:04 +0200

    version 1.0.0

--- packaging_tutorial/report_FEDONYUK/report_api.py
+++ packaging_tutorial/report_FEDONYUK/report_api.py
@@ -2,8 +2,11 @@
 from flask import request, make_response
 from flask_restful import Resource
 from dicttoxml import dicttoxml
+from loguru import logger
 
 from packaging_tutorial.report_FEDONYUK.db_util import get_report, get_drivers
+
+logger.add('debug.log', colorize=True, format='{time} {level} {message}', level='DEBUG')
 
 
 def generate_response(report, report_format):
@@ -15,27 +18,36 @@ def generate_response(report, report_format):
         headers = {'Content-Type': 'application/xml'}
         response = make_response(xml)
         response.headers = headers
-        return response
+        return response, 200
+    logger.error("[ERROR] Invalid format requested.")
     return {'error': 'Invalid format'}, 400
 
 
 class ReportResource(Resource):
     def get(self):
-        report_format = request.args.get('format', 'json')
-        order = request.args.get('order', 'asc')
-        driver_id = request.args.get('driver_id', None)
-        report = get_report(asc=(order != 'desc'), driver=driver_id)
-
-        return generate_response(report, report_format)
+        try:
+            report_format = request.args.get('format', 'json')
+            order = request.args.get('order', 'asc')
+            driver_id = request.args.get('driver_id', None)
+            report = get_report(asc=(order != 'desc'), driver=driver_id)
+            logger.info("[INFO] Report data retrieved successfully.")
+            return generate_response(report, report_format)
+        except Exception as e:
+            logger.error(f"[ERROR] An error occurred in ReportResource: {e}")
+            return {'error': 'An error occurred'}, 500
 
 
 class DriversResource(Resource):
     def get(self):
-        report_format = request.args.get('format', 'json')
-        order = request.args.get('order', 'asc')
-        drivers_list = get_drivers(asc=(order != 'desc'))
-        driver_id = request.args.get('driver_id', None)
-        if 'driver_id' in request.args:
-            drivers_list = get_report(driver=driver_id)
-
-        return generate_response(drivers_list, report_format)
+        try:
+            report_format = request.args.get('format', 'json')
+            order = request.args.get('order', 'asc')
+            drivers_list = get_drivers(asc=(order != 'desc'))
+            driver_id = request.args.get('driver_id', None)
+            if 'driver_id' in request.args:
+                drivers_list = get_report(driver=driver_id)
+            logger.info("Drivers data retrieved successfully.")
+            return generate_response(drivers_list, report_format)
+        except Exception as e:
+            logger.error(f"An error occurred in DriversResource: {e}")
+            return {'error': 'An error occurred'}, 500
